a930c06ebe867d2839f87856cf03e277fd622eea
rusli abdul gani <rusli.gani88@gmail.com>
2017-07-19 18:38:12 +0700

    fix gitignore

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,47 @@ router.get('/', (req, res) => {
   })
 })
 
+router.get('/add', (req, res) => {
+  db.User.findAll()
+  .then( data => {
+    res.render('user_add', {title: 'user add page', data_users: data})
+  })
+})
+
+router.post('/add', (req, res) =>{
+  db.User.create(req.body)
+  .then( () => {
+    res.redirect('/users')
+  })
+})
+
+router.get('/delete/:id', (req, res) => {
+  db.User.destroy({
+    where: {id: req.params.id}
+  })
+  .then( () => {
+    res.redirect('/users')
+  })
+})
+
+
+router.get('/edit/:id', (req, res) => {
+  db.User.findById(req.params.id)
+  .then( data => {
+    res.render('edit_user', { edit_user: data})
+  })
+})
+
+router.post('/edit/:id', (req, res) => {
+  db.User.update(
+    req.body
+  ,{
+    where:{id:req.params.id}
+  })
+  .then( ()=>{
+    res.redirect('/users')
+  })
+})
 
 
 
